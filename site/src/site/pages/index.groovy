43b7f0b6412b4a3ee43a6c686de199f535670113
Marco Vermeulen <vermeulen.mp@gmail.com>
2015-06-13 20:14:10 +0200

    Fixes layout removing events and changing page title.

--- site/src/site/pages/index.groovy
+++ site/src/site/pages/index.groovy
@@ -1,34 +1,10 @@
-import model.Event
-
 layout 'layouts/main.groovy', true,
-        pageTitle: 'The Groovy programming language',
+        pageTitle: 'SDKman the Software Development Kit Manager',
         mainContent: contents {
             div(id: 'band', class: 'band') {
 
             }
             div(id: 'content') {
                 include unescaped: 'html/index.html'
-
-                section(class: "row colset-3-article") {
-                    h1 { strong "Groovy events you shouldn't miss!" }
-                    allEvents.keySet().take(3).each { String eventName ->
-                        Event event = allEvents[eventName]
-                        article {
-                            div(class: 'content') {
-                                // Note that the event image should be 257x180 to look nice
-                                div(class: 'event-img', style: "background-image: url(${event.logo})") {}
-                                h1 {
-                                    a(href: event.url) {
-                                        strong eventName
-                                        br()
-                                        em event.location
-                                    }
-                                }
-                                time event.date
-                                yieldUnescaped event.description
-                            }
-                        }
-                    }
-                }
             }
         }
