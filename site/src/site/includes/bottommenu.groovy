9c37afcea0f62501e92dbba784ffe02c84b7c5c4
Marco Vermeulen <vermeulen.mp@gmail.com>
2015-07-08 22:26:40 +0100

    Remove sitemap from page footer.

--- site/src/site/includes/bottommenu.groovy
+++ site/src/site/includes/bottommenu.groovy
@@ -2,19 +2,6 @@
 // footer
 footer(id: 'footer') {
     div(class: 'row') {
-        div(class: 'colset-3-footer') {
-            menu.entrySet().eachWithIndex { entry, i ->
-                def (name, menu) = [entry.key, entry.value]
-                div(class: "col-${i+1}") {
-                    h1(name)
-                    ul {
-                        menu.each { menuItem ->
-                            li { a(href: relative(menuItem.link), menuItem.name) }
-                        }
-                    }
-                }
-            }
-        }
         div(class: 'clearfix', "&copy; 2012-${Calendar.instance[Calendar.YEAR]} SDKman/GVM is Open Source Software licensed under ${$a(href: 'http://www.apache.org/licenses/LICENSE-2.0.html', 'Apache 2')}")
     }
 }
