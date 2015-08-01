bf75a574f36362deaf81de9b948ce7ba8a1892bc
Marco Vermeulen <vermeulen.mp@gmail.com>
2015-07-31 19:47:23 -0500

    Introducing install page.

--- site/src/site/pages/install.groovy
+++ site/src/site/pages/install.groovy
@@ -1,12 +1,38 @@
 layout 'layouts/main.groovy', true,
         pageTitle: 'Installation - SDKman',
         mainContent: contents {
-            div(id: 'band', class: 'band') {
+            div(id: 'content', class: 'page-1') {
+                div(class: 'row') {
+                    div(class: 'row-fluid') {
+                        div(class: 'col-lg-3') {
+                            include unescaped: 'html/twittersearch.html'
+                        }
 
-            }
-            div(id: 'content') {
-                section {
-                    p('This is the Install Page.')
+                        div(class: 'col-lg-8 col-lg-pull-0') {
+                            h1 {
+                                i(class: 'fa fa-cloud-download') {}
+                                yield ' Installation'
+                            }
+                            article {
+                                p {
+                                    yield '''Installing Software Development Kits (SDKs) on any UNIX-like platform
+                                             has never been easier. SDKman installs smoothly on Mac OSX, Linux, Cygwin,
+                                             Solaris and FreeBSD. We also support both Bash and ZSH.'''
+                                    br()
+                                    yield 'Simply open a new terminal and enter: '
+                                }
+                                pre { code '$ curl -s get.gvmtool.net | bash' }
+                                p {
+                                    yield 'Follow the instructions on-screen to complete installation.'
+                                    br()
+                                    yield 'Next, open a new terminal or type the command:'
+                                }
+                                pre { code '$ source "$HOME/.gvm/bin/gvm-init.sh"' }
+                                p 'That\'s all there is to it!'
+                            }
+                            hr(class: 'divider')
+                        }
+                    }
                 }
             }
         }
