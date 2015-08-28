2a6839814f4adab1aded5095c9e9a7477a9b7f3c
Marco Vermeulen <vermeulen.mp@gmail.com>
2015-08-28 15:23:03 +0100

    Tweaks to Usage navbar.

--- site/src/site/includes/usage-navbar.groovy
+++ site/src/site/includes/usage-navbar.groovy
@@ -1,47 +1,17 @@
 ul(class: 'nav-sidebar') {
-    li(class: 'active') {
-        a(href: 'usage.html') { strong('Usage') }
-    }
-    li {
-        a(href: '#installdefault', class: 'anchor-link', 'Install Latest Stable')
-    }
-    li {
-        a(href: '#installspecific', class: 'anchor-link', 'Install Specific Version')
-    }
-    li {
-        a(href: '#remove', class: 'anchor-link', 'Remove Version')
-    }
-    li {
-        a(href: '#list', class: 'anchor-link', 'List Versions')
-    }
-    li {
-        a(href: '#use', class: 'anchor-link', 'Use Version')
-    }
-    li {
-        a(href: '#default', class: 'anchor-link', 'Default Version')
-    }
-    li {
-        a(href: '#current', class: 'anchor-link', 'Current Version')
-    }
-    li {
-        a(href: '#outdated', class: 'anchor-link', 'Outdated Version')
-    }
-    li {
-        a(href: '#version', class: 'anchor-link', 'sdkman Version')
-    }
-    li {
-        a(href: '#broadcast', class: 'anchor-link', 'Broadcast Messages')
-    }
-    li {
-        a(href: '#offline', class: 'anchor-link', 'Offline Mode')
-    }
-    li {
-        a(href: '#selfupdate', class: 'anchor-link', 'Self-Update')
-    }
-    li {
-        a(href: '#flush', class: 'anchor-link', 'Flush')
-    }
-    li {
-        a(href: '#help', class: 'anchor-link', 'Getting Help')
-    }
+    li(class: 'active') { a(href: 'usage.html') { strong('Usage') } }
+    li { a(href: '#installdefault', class: 'anchor-link', 'Install Latest Stable') }
+    li { a(href: '#installspecific', class: 'anchor-link', 'Install Specific Version') }
+    li { a(href: '#remove', class: 'anchor-link', 'Remove Version') }
+    li { a(href: '#list', class: 'anchor-link', 'List Versions') }
+    li { a(href: '#use', class: 'anchor-link', 'Use Version') }
+    li { a(href: '#default', class: 'anchor-link', 'Default Version') }
+    li { a(href: '#current', class: 'anchor-link', 'Current Version') }
+    li { a(href: '#outdated', class: 'anchor-link', 'Outdated Version') }
+    li { a(href: '#version', class: 'anchor-link', 'Version') }
+    li { a(href: '#broadcast', class: 'anchor-link', 'Broadcast Messages') }
+    li { a(href: '#offline', class: 'anchor-link', 'Offline Mode') }
+    li { a(href: '#selfupdate', class: 'anchor-link', 'Self-Update') }
+    li { a(href: '#flush', class: 'anchor-link', 'Flush') }
+    li { a(href: '#help', class: 'anchor-link', 'Help') }
 }
