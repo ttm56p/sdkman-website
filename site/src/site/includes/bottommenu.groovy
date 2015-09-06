dc2e389bca86e13916d0806c2b3a8804373441f1
Marco Vermeulen <vermeulen.mp@gmail.com>
2015-09-06 18:36:04 +0100

    Updated credit on footer.

--- site/src/site/includes/bottommenu.groovy
+++ site/src/site/includes/bottommenu.groovy
@@ -2,6 +2,6 @@
 footer(id: 'footer') {
     div(class: 'row') {
         div(class: 'clearfix', "&copy; 2012-${Calendar.instance[Calendar.YEAR]} sdkman/GVM is Open Source Software licensed under ${$a(href: 'http://www.apache.org/licenses/LICENSE-2.0.html', 'Apache 2')}")
-        div(class: 'clearfix', "Logos and Additional Design by ${$a(href: 'https://github.com/dmesu', 'Daida Medina')}")
+        div(class: 'clearfix', "Logos and additional Design by ${$a(href: 'https://github.com/dmesu', 'Daida Medina')}")
     }
 }
