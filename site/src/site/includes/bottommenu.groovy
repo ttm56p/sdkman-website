18f22d5b75416f60c9923b615e2a6a79ec82858a
Marco Vermeulen <vermeulen.mp@gmail.com>
2015-09-06 17:40:16 +0100

    Add logo and design credit.

--- site/src/site/includes/bottommenu.groovy
+++ site/src/site/includes/bottommenu.groovy
@@ -1,7 +1,7 @@
-
 // footer
 footer(id: 'footer') {
     div(class: 'row') {
         div(class: 'clearfix', "&copy; 2012-${Calendar.instance[Calendar.YEAR]} sdkman/GVM is Open Source Software licensed under ${$a(href: 'http://www.apache.org/licenses/LICENSE-2.0.html', 'Apache 2')}")
+        div(class: 'clearfix', "Logos and Additional Design by ${$a(href: 'https://github.com/dmesu', 'Daida Medina')}")
     }
 }
