551092ab7e11e9a40ef78f2532260672482ae1f4
Marco Vermeulen <vermeulen.mp@gmail.com>
2015-09-19 10:59:07 +0100

    Adds git config.

--- bin/deploy_static.sh
+++ bin/deploy_static.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 ./gradlew clean generateSite
 cd site/build/site
+git config user.email "build@example.org"
+git config user.name "Codeship Build"
 git init
 git add --all
 git commit -m 'Committed on CI.'
 git remote add origin git@github.com:sdkman/sdkman.github.io.git
-git push origin master
+git push -f origin master
