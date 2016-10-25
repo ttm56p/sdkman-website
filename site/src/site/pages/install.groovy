d02764630e37464a5f97fb5b809a9d3ddc33d8cd
Marco Vermeulen <vermeulen.mp@gmail.com>
2016-10-25 16:55:50 +0100

    Add beta channel section.

--- site/src/site/pages/install.groovy
+++ site/src/site/pages/install.groovy
@@ -33,6 +33,23 @@ layout 'layouts/main.groovy', true,
                                 pre { code '$ sdk version' }
                                 p 'If all went well, you should see the version information:'
                                 pre { code '  sdkman 5.0.0+51' }
+                            }
+
+                            h3 {
+                                yield 'Beta Channel'
+                            }
+                            article {
+                                p {
+                                    yield '''For the more adventurous among us, we have a beta channel.
+                                         All new CLI features will first be rolled out to this group of users for trial purposes.
+                                         These versions can be considered stable for the most part, but might occasionally break.
+                                         To join the program, simply update the the '''
+                                    code '~/.sdkman/etc/config'
+                                    yield ' file to as follows:'
+                                    br()
+                                    code 'sdkman_beta_channel=true'
+                                }
+
                                 p {
                                     yield 'That\'s all there is to it! Next we will look at '
                                     a(href: "/usage.html", 'Usage')
@@ -40,22 +57,21 @@ layout 'layouts/main.groovy', true,
                                 }
                             }
 
+
+                            h3 {
+                                yield 'Installing to a Custom Location'
+                            }
                             article {
-                                h3 {
-                                    yield 'Installing to a Custom Location'
-                                }
-                                p {
-                                    yield 'It is possible to install SDKMAN! to a custom location other than '
-                                    code '$HOME/.sdkman'
-                                    yield '. This can be achieved by exporting your custom location as '
-                                    code 'SDKMAN_DIR'
-                                    yield ' prior to installing.'
-                                    br()
-                                    yield 'Simply open a new terminal and enter:'
-                                    pre { code '$ export SDKMAN_DIR="/usr/local/sdkman" && curl -s "https://get.sdkman.io" | bash' }
-                                    yield 'For this to work it is vital that your user has full access rights to this folder.'
-                                    yield 'It is also important that the folder does not exist as SDKMAN! will attempt to create it.'
-                                }
+                                yield 'It is possible to install SDKMAN! to a custom location other than '
+                                code '$HOME/.sdkman'
+                                yield '. This can be achieved by exporting your custom location as '
+                                code 'SDKMAN_DIR'
+                                yield ' prior to installing.'
+                                br()
+                                yield 'Simply open a new terminal and enter:'
+                                pre { code '$ export SDKMAN_DIR="/usr/local/sdkman" && curl -s "https://get.sdkman.io" | bash' }
+                                yield 'For this to work it is vital that your user has full access rights to this folder.'
+                                yield 'It is also important that the folder does not exist as SDKMAN! will attempt to create it.'
                             }
                         }
                     }
