fb98de736c6ef2b6574075e70c0af163577c9fde
Marco Vermeulen <vermeulen.mp@gmail.com>
2018-05-07 13:51:55 +0100

    Use RatpackSugar in ContactFormHandler.

--- src/main/scala/io/sdkman/site/ContactFormHandler.scala
+++ src/main/scala/io/sdkman/site/ContactFormHandler.scala
@@ -2,13 +2,14 @@ package io.sdkman.site
 
 import ratpack.form.Form
 import ratpack.handling.{Context, Handler}
+import support.RatpackSugar
 
-class ContactFormHandler extends Handler with Email {
+class ContactFormHandler extends Handler with RatpackSugar with Email {
 
   override def handle(ctx: Context): Unit = {
     ctx.parse(classOf[Form]).blockingOp { f =>
       send(f.get("email"), f.get("name"), f.get("message"))
-    } then { _ =>
+    } andThen { _ =>
       ctx.redirect("/")
     }
   }
