c059c7ed3507d39992b64fe5e9fd6f9324e26546
Marco Vermeulen <vermeulen.mp@gmail.com>
2018-05-14 20:06:22 +0100

    Implement client side recaptcha.

--- src/main/scala/io/sdkman/site/ContactFormHandler.scala
+++ src/main/scala/io/sdkman/site/ContactFormHandler.scala
@@ -8,6 +8,9 @@ class ContactFormHandler extends Handler with RatpackSugar with Email {
 
   override def handle(ctx: Context): Unit = {
     ctx.parse(classOf[Form]).blockingOp { f =>
+      val recaptchaResponse = f.get("g-recaptcha-response")
+      val ipAddress = ctx.getRequest.getHeaders.get("X-Real-IP")
+      println(s"Recaptcha: $recaptchaResponse $ipAddress")
       send(f.get("email"), f.get("name"), f.get("message"))
     } andThen { _ =>
       ctx.redirect("//")
--- src/main/scala/io/sdkman/site/ContextualHandler.scala
+++ src/main/scala/io/sdkman/site/ContextualHandler.scala
@@ -4,14 +4,17 @@ import ratpack.handling.Context
 import support.{Handler, OK}
 
 class ContextualHandler extends Handler {
+
+  val RecaptchaSiteKey = "6LfvzlcUAAAAAHIPCKV1E4ZFsd-PKagAVDyXF2mR"
+
   override def handles(implicit ctx: Context): Unit = {
     Option(ctx.getAllPathTokens.get("context")) match {
-      case Some("index") => OK(html.index())
+      case Some("index") => OK(html.index(RecaptchaSiteKey))
       case Some("install") => OK(html.install())
       case Some("usage") => OK(html.usage())
       case Some("vendors") => OK(html.vendors())
       case Some(_) => ctx.notFound()
-      case None => OK(html.index())
+      case None => OK(html.index(RecaptchaSiteKey))
     }
   }
 }
