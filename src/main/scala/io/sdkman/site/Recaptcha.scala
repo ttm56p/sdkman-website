b9710f408271ee07082684842c3c59a87708ded4
Marco Vermeulen <vermeulen.mp@gmail.com>
2018-05-19 17:55:01 +0100

    Add useful logging.

--- src/main/scala/io/sdkman/site/Recaptcha.scala
+++ src/main/scala/io/sdkman/site/Recaptcha.scala
@@ -1,5 +1,6 @@
 package io.sdkman.site
 
+import com.typesafe.scalalogging.LazyLogging
 import io.circe.generic.auto._
 import io.circe.parser._
 import ratpack.exec.Promise
@@ -8,13 +9,15 @@ import ratpack.http.client.{HttpClient, ReceivedResponse}
 
 trait Recaptcha extends {
 
-  self: Configuration =>
+  self: Configuration with LazyLogging =>
 
   def recaptcha(request: RecaptchaRequest)(implicit ctx: Context): Promise[RecaptchaResponse] = {
     ctx.get(classOf[HttpClient]).post(recaptchaUrl, spec => spec.getBody.text(request.toString)).map[RecaptchaResponse] { response: ReceivedResponse =>
-      decode[RecaptchaResponse](response.getBody.getText).fold(
+      val rresp = decode[RecaptchaResponse](response.getBody.getText).fold(
         e => RecaptchaResponse(success = false, `error-codes` = Some(e.getMessage)),
         rr => if (!rr.success) throw new Exception(rr.`error-codes`.mkString(";")) else rr)
+      logger.info(s"Recaptcha responded: ${rresp.toString}")
+      rresp
     }
   }
 
