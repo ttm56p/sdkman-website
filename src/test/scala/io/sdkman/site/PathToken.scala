37ee16c212d725de11e68c8c4e509ffcee50eda3
Marco Vermeulen <vermeulen.mp@gmail.com>
2018-05-20 15:43:31 +0100

    Enable some tests, remove obsolete test collaborator.

--- src/test/scala/io/sdkman/site/PathToken.scala
+++
@@ -1,9 +0,0 @@
-package io.sdkman.site
-
-import java.util
-
-object PathToken {
-  def binding(key: String, value: String): util.HashMap[String, String] = new util.HashMap[String, String] {
-    put(key, value)
-  }
-}
